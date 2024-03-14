791f5a6da40b6ffd263fa42914a1b0de2165d694
Patryk <rastawafel03@gmail.com>
2024-03-14 21:47:01 +0100

    Created routes and handlers

--- main.go
+++ main.go
@@ -1,29 +1,16 @@
 package main
 
 import (
+	"context"
 	"fmt"
-	"github.com/go-chi/chi/v5"
-	"github.com/go-chi/chi/v5/middleware"
-	"net/http"
+	"main/application"
 )
 
 func main() {
+	app := application.New()
 
-	router := chi.NewRouter()
-	router.Use(middleware.Logger)
-
-	router.Get("/hello", basicHandler)
-
-	server := &http.Server{
-		Addr:    ":3000",
-		Handler: router,
-	}
-
-	err := server.ListenAndServe()
+	err := app.Start(context.TODO())
 	if err != nil {
-		fmt.Print("failed to listen to server", err)
+		fmt.Print("failed to start the server: %w", err)
 	}
-}
-func basicHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello, world!"))
 }
